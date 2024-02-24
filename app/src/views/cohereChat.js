fd2cf23e7e77cb7c3d5ce1fc1c5de4de1bd2c69c
Your Name <99445200+ashsic@users.noreply.github.com>
2024-02-24 17:54:27 -0500

    cohere generate API functional

--- app/src/views/cohereChat.js
+++ app/src/views/cohereChat.js
@@ -6,25 +6,53 @@ function ChatRoom() {
   
 
   const cohere = new CohereClient({
-    token: "VWR26LnBwuy2gQ6xgb40MgSZ2FrMlBMmoceGrUq3", // This is your trial API key
+    token: "CmTSYM5W6eTKpPlGPoLxOYrp9SkIu8qmdNSaKaFJ", 
   });
 
-  (async () => {
-    const stream = await cohere.chatStream({
-      model: "command",
-      message: "<YOUR MESSAGE HERE>",
-      chatHistory: [{"role": "User", "message": "give me a list of housing resources for black students in toronto"}],
-      promptTruncation: "AUTO",
-      citationQuality: "accurate",
-      connectors: [{"id":"web-search"}],
-      documents: []
-    });
+  // (async () => {
+  //   const stream = await cohere.chatStream({
+  //     model: "command",
+  //     message: "<YOUR MESSAGE HERE>",
+  //     chatHistory: [{"role": "User", "message": "give me a list of housing resources for black students in toronto"}],
+  //     promptTruncation: "AUTO",
+  //     citationQuality: "accurate",
+  //     connectors: [{"id":"web-search"}],
+  //     documents: []
+  //   });
 
-    for await (const chat of stream) {
-        if (chat.eventType === "text-generation") {
-            process.stdout.write(chat.text);
-        }
-    }
+  //   for await (const chat of stream) {
+  //       if (chat.eventType === "text-generation") {
+  //           process.stdout.write(chat.text);
+  //       }
+  //   }
+  // })();
+
+
+  // (async () => {
+  //   const chatStream = await cohere.chatStream({
+  //       chatHistory: [
+  //           { role: "USER", message: "Who discovered gravity?" },
+  //           { role: "CHATBOT", message: "The man who is widely credited with discovering gravity is Sir Isaac Newton" }
+  //       ],
+  //       message: "What year was he born?",
+  //       // perform web search before answering the question. You can also use your own custom connector.
+  //       connectors: [{ id: "web-search" }],
+  //   });
+
+  //   for await (const message of chatStream) {
+  //       if (message.eventType === "text-generation") {
+  //           console.log(message);
+  //       }
+  //   }
+  // })();
+
+  (async () => {
+    const prediction = await cohere.generate({
+        prompt: "hello",
+        maxTokens: 10,
+    });
+    
+    console.log("Received prediction", prediction);
   })();
 
 
@@ -37,10 +65,15 @@ function ChatRoom() {
       <h1>Chat Room</h1>
       <div>
         <p>Welcome to the Chat Room!</p>
-        <button onClick={() => console.log('/chat')}>Start Chatting</button>
+        <form>
+          
+          <button onClick={() => console.log('/chat')}>Send Message</button>
+        </form>
+        
       </div>
     </div>
   );
+  
 }
 
 export default ChatRoom;
