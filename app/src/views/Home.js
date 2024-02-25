d176acea0ad2584cb9e9880f875a11dd2a43dd4e
Mayamba Boka <bokambma@sheridancollege.ca>
2024-02-24 21:17:17 -0500

    Made css pages and will start styling

--- app/src/views/Home.js
+++ app/src/views/Home.js
@@ -5,9 +5,25 @@ class HomePage extends Component {
     render() {
         return (
             <div>
-                <h1>Welcome to ____</h1>
-                <div>
-                    
+                <h1>Welcome (Name Placeholder)</h1>
+                <div className="scholar">
+                     <Link  to="/views/Grants">Grants and Scholarships</Link>
+                </div>
+
+                <div className="research">
+                    <Link  to="/views/Research">Educational Papers</Link>
+                </div>
+                <div className="health">
+                    <Link  to="">Mental Health</Link>
+                </div>
+                <div className="houseing">
+                    <Link  to="/views/">Housing</Link>
+                </div>
+                <div className="social">
+                    <Link  to="/views/">Social Eats</Link>
+                </div>
+                <div className="community">
+                    <Link  to="/views/">Find Community</Link>
                 </div>
             </div>
         );
